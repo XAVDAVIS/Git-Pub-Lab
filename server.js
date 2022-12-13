a043096debded6801fa6e83c7294f7fca6acf114
Xavier <shottyx5156@hotmail.com>
2022-12-13 01:01:53 -0500

    Adds ejs files

--- server.js
+++ server.js
@@ -1,9 +1,22 @@
 const express = require("express")
 const app = express()
+const drinks = require("./models/drinks")
+
 const port = 3000
 
 app.get("", (req, res) => {
     res.send('Welcome to the Gitpub App!')
+})
+
+app.get("/drinks/", (req, res) => {
+    res.render("drinks_index.ejs", {
+        allDrinks: drinks,
+    })
+})
+app.get("/drinks/:indexOfDrinksArray", (req, res ) => {
+    res.render("drinks_show.ejs", {
+        drink: drinks[req.params.indexOfDrinksArray],
+    })
 })
 
 app.listen(3000, () => {
